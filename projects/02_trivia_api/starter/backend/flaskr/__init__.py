6249a35040cbb9ceea0e21ae182cf3aef61a9ded
alexxsanya <alexxsanya@gmail.com>
2019-11-20 06:27:28 +0300

    add error handlers

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -167,6 +167,46 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'Bad Request'
+    }), 400
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Resource Not Found'
+    }), 404
+
+  @app.errorhandler(405)
+  def method_not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'Method Not Allowed'
+    }), 405
+
+  @app.errorhandler(422)
+  def unprocessed(error):
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'Request Not Processed'
+    }), 422
+
+  @app.errorhandler(500)
+  def server_error(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'Server Error'
+    }), 500
+
   
   return app
 
